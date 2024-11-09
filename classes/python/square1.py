ddd26a4fddbe6caf35d8c6546b4997906d6b87e9
Thomas Daniel Galligan <thomas007g@gmail.com>
2024-11-09 09:05:35 +0000

    added python pages and scripts

---
+++ classes/python/square1.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+def main():
+    t = Turtle()
+
+    t.forward(90)
+    t.right(90)
+    t.forward(90)
+    t.right(90)
+    t.forward(90)
+    t.right(90)
+    t.forward(90)
+    
+    input()
+    
+if __name__ == "__main__":
+    main()
---
+++ classes/python/square2.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+def main():
+    t = Turtle()
+
+    for _ in range(4):
+        t.forward(90)
+        t.right(90)
+        
+    input()
+
+if __name__ == "__main__":
+    main()
